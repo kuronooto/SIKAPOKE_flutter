cadb392f7b02759e59140ed4bdc9f7cb3c5feec4
Yanai1005 <tatsuya871_1005@icloud.com>
2025-03-15 13:46:48 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sikapoke_flutter
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/sikapoke_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sikapoke_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
