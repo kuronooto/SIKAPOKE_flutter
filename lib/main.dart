d17059774fd014b66c3385956045f389db1cd043
RiTa-23 <rita.tifrt@gmail.com>
2025-03-22 18:16:41 +0900

    fix:データアップロード用の状態からもとに戻した

--- lib/main.dart
+++ lib/main.dart
@@ -33,15 +33,14 @@ class AuthCheck extends StatelessWidget {
     return FutureBuilder<User?>(
       future: _checkLoginStatus(),
       builder: (context, snapshot) {
-        return DataUploadPage();
-        // if (snapshot.connectionState == ConnectionState.waiting) {
-        //   return Scaffold(body: Center(child: CircularProgressIndicator()));
-        // } else if (snapshot.hasData) {
-        //   return RootPage();
-        // } else {
-        //   //return DataUploadPage();
-        //   return LoginPage();
-        // }
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Scaffold(body: Center(child: CircularProgressIndicator()));
+        } else if (snapshot.hasData) {
+          return RootPage();
+        } else {
+          //return DataUploadPage();
+          return LoginPage();
+        }
       },
     );
   }
