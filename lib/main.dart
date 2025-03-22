51dbbc755235e2b5b05a85869a071cd9879526fc
リタ <rita@ritanoMacBook-Air.local>
2025-03-22 10:22:55 +0900

    add:ログインチェック追加・minSdkVersion設定

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart'; // 追加: Firebase Core のインポート
 import 'package:flutter/material.dart';
 import 'package:sikapoke_flutter/firebase_options.dart';
 import 'pages/root_page.dart';
+import 'pages/auth_page.dart'; // 追加: LoginPage のインポート
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,8 +16,29 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: RootPage(), // 変更: LoginPage に遷移
+    return MaterialApp(home: AuthCheck());
+  }
+}
+
+class AuthCheck extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<User?>(
+      future: _checkLoginStatus(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Scaffold(body: Center(child: CircularProgressIndicator()));
+        } else if (snapshot.hasData) {
+          return RootPage();
+        } else {
+          //return DataUploadPage();
+          return LoginPage();
+        }
+      },
     );
+  }
+
+  Future<User?> _checkLoginStatus() async {
+    return FirebaseAuth.instance.currentUser;
   }
 }
