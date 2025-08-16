0a79558792e74497177d29455f4c9b1c4b9275a1
tyumu <115434121+tyumu@users.noreply.github.com>
2025-08-17 00:47:35 +0900

    コンフリ修正（主にバトルルール

--- lib/pages/card_page.dart
+++ lib/pages/card_page.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'dart:math' as math;
-import 'package:sikapoke_flutter/pages/gacha/widgets/gacha_utils.dart';
 import 'Deckbuilder_page.dart';
 
 class CardPage extends StatelessWidget {
